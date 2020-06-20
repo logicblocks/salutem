ec408b298699ac711f2327eeecd39234c0901e63
Toby Clemson <tobyclemson@gmail.com>
2020-06-20 01:04:41 +0100

    Add basic check structure.

--- src/salutem/core.clj
+++ src/salutem/core.clj
@@ -1,1 +1,50 @@
 (ns salutem.core)
+
+(defn empty-registry []
+  {:checks  {}
+   :results {}})
+
+(defn check
+  ([name check-fn] (check name check-fn {}))
+  ([name check-fn
+    {:keys [asynchronous]
+     :or   {asynchronous true}}]
+   {:name         name
+    :check-fn     check-fn
+    :asynchronous asynchronous}))
+
+(defn asynchronous? [check]
+  (:asynchronous check))
+
+(defn synchronous? [check]
+  (not (:asynchronous check)))
+
+(defn result
+  ([status] (result status {}))
+  ([status {:keys []}]
+   {:status status}))
+
+(defn with-check [registry check]
+  (update-in registry [:checks] assoc (:name check) check))
+
+(defn with-result [registry check result]
+  (update-in registry [:results] assoc (:name check) result))
+
+(defn find-check [registry name]
+  (get-in registry [:checks name]))
+
+(defn find-result [registry name]
+  (get-in registry [:results name]))
+
+(defn check-names [registry]
+  (set (keys (:checks registry))))
+
+(defn resolve-check
+  ([registry name]
+   (resolve-check registry name {}))
+  ([registry name context]
+   (let [check (find-check registry name)
+         result (find-result registry name)]
+     (if (or (synchronous? check) (not result))
+       ((:check-fn check) context)
+       result))))
--- test/salutem/core_test.clj
+++ test/salutem/core_test.clj
@@ -1,6 +1,64 @@
 (ns salutem.core-test
   (:require
-   [clojure.test :refer :all]))
+   [clojure.test :refer :all]
 
-(deftest example
-  (is (= 1 1)))
+   [salutem.core :as salutem]))
+
+; use a single atom (registry) to store checks and results
+; provide functions to look up parts of the registry
+;   - if check being resolved is async, fetch latest result
+;   - if check being resolved is sync, execute to get result and update registry
+; use go routine on timer to run through and run checks that need running
+; when async checks run, update registry in atom
+
+(deftest adds-single-check-to-registry
+  (let [check (salutem/check :thing (fn [_] (salutem/result :healthy)))
+        registry (salutem/with-check (salutem/empty-registry) check)]
+    (is (= (salutem/find-check registry :thing) check))))
+
+(deftest adds-many-checks-to-registry
+  (let [check-1 (salutem/check :thing-1 (fn [_] (salutem/result :healthy)))
+        check-2 (salutem/check :thing-2 (fn [_] (salutem/result :unhealthy)))
+
+        registry (-> (salutem/empty-registry)
+                   (salutem/with-check check-1)
+                   (salutem/with-check check-2))]
+   (is (= (salutem/check-names registry) #{:thing-1 :thing-2}))))
+
+(deftest resolves-synchronous-check-when-no-previous-result-available
+  (let [check (salutem/check :thing
+                (fn [_] (salutem/result :healthy))
+                {:asynchronous false})
+        registry (salutem/with-check (salutem/empty-registry) check)]
+    (is (= (salutem/resolve-check registry :thing)
+          (salutem/result :healthy)))))
+
+(deftest resolves-synchronous-check-even-when-previous-result-available
+  (let [check (salutem/check :thing
+                (fn [_] (salutem/result :unhealthy))
+                {:asynchronous false})
+        result (salutem/result :healthy)
+        registry (-> (salutem/empty-registry)
+                   (salutem/with-check check)
+                   (salutem/with-result check result))]
+    (is (= (salutem/resolve-check registry :thing)
+          (salutem/result :unhealthy)))))
+
+(deftest resolves-asynchronous-check-when-no-previous-result-available
+  (let [check (salutem/check :thing
+                (fn [_] (salutem/result :unhealthy))
+                {:asynchronous true})
+        registry (salutem/with-check (salutem/empty-registry) check)]
+    (is (= (salutem/resolve-check registry :thing)
+          (salutem/result :unhealthy)))))
+
+(deftest resolves-asynchronous-check-as-previous-result-when-available
+  (let [check (salutem/check :thing
+                (fn [_] (salutem/result :unhealthy))
+                {:asynchronous true})
+        result (salutem/result :healthy)
+        registry (-> (salutem/empty-registry)
+                   (salutem/with-check check)
+                   (salutem/with-result check result))]
+    (is (= (salutem/resolve-check registry :thing)
+          (salutem/result :healthy)))))
