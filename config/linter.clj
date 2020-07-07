790f91783133ed576552a4c1f32636a0c394ff9c
Toby Clemson <tobyclemson@gmail.com>
2020-07-08 00:45:06 +0100

    Turn off linter rule in async/go macro.

--- config/linter.clj
+++ config/linter.clj
@@ -1,5 +1,7 @@
 (disable-warning
   {:linter :suspicious-expression
    :for-macro 'clojure.core/or
-   :if-inside-macroexpansion-of #{'clojure.core.async/alt!!}
-   :within-depth 13})
+   :if-inside-macroexpansion-of #{'clojure.core.async/go
+                                  'clojure.core.async/alt!
+                                  'clojure.core.async/alt!!}
+   :within-depth 18})
