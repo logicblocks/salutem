8065b3a496eb554a02c22bd4b2cdb6bcb490d412
Circle CI <circleci@infrablocks.io>
2020-07-26 12:51:45 +0000

    Pre-release version 0.1.0-RC8 [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/salutem "0.1.0-RC7"
+(defproject io.logicblocks/salutem "0.1.0-RC8"
   :description "A health check library for sync / async health checks."
   :url "https://github.com/logicblocks/salutem"
 
