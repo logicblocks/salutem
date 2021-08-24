cfabb9f1e3f3d3d8700299832801b1c60fd27dac
Circle CI <circleci@infrablocks.io>
2021-08-24 10:39:14 +0000

    Pre-release version 0.1.0-RC21 [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/salutem "0.1.0-RC20"
+(defproject io.logicblocks/salutem "0.1.0-RC21"
   :description "Parent for all salutem modules."
 
   :plugins [[lein-modules "0.3.11"]
