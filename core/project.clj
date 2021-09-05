b9cc2c8c3dde6978cd14268b45a6270dda659496
Circle CI <circleci@infrablocks.io>
2021-09-05 13:21:09 +0000

    Pre-release version 0.1.2-RC7 [skip ci]

--- core/project.clj
+++ core/project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/salutem.core "0.1.2-RC6"
+(defproject io.logicblocks/salutem.core "0.1.2-RC7"
   :description "A health check library for sync / async health checks."
 
   :plugins [[lein-modules "0.3.11"]]
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/salutem "0.1.2-RC6"
+(defproject io.logicblocks/salutem "0.1.2-RC7"
   :description "Parent for all salutem modules."
 
   :plugins [[lein-modules "0.3.11"]
