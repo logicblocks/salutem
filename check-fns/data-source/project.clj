09aedb1bc190c4fae2921f79b857bdfa98412c0f
Circle CI <circleci@infrablocks.io>
2021-10-11 00:52:19 +0000

    Pre-release version 0.1.7-RC2 [skip ci]

--- check-fns/data-source/project.clj
+++ check-fns/data-source/project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/salutem.check-fns.data-source "0.1.7-RC1"
+(defproject io.logicblocks/salutem.check-fns.data-source "0.1.7-RC2"
   :description "A data source check function for salutem."
 
   :plugins [[lein-modules "0.3.11"]]
--- core/project.clj
+++ core/project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/salutem.core "0.1.7-RC1"
+(defproject io.logicblocks/salutem.core "0.1.7-RC2"
   :description "A health check library for sync / async health checks."
 
   :plugins [[lein-modules "0.3.11"]]
