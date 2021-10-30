06261e74d9b6763269fb409ecee0b8f73812adc9
Circle CI <circleci@infrablocks.io>
2021-10-30 19:02:28 +0000

    Pre-release version 0.1.7-RC15 [skip ci]

--- check-fns/data-source/project.clj
+++ check-fns/data-source/project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/salutem.check-fns.data-source "0.1.7-RC14"
+(defproject io.logicblocks/salutem.check-fns.data-source "0.1.7-RC15"
   :description "A data source check function for salutem."
 
   :parent-project {:path    "../../parent/project.clj"
--- check-fns/project.clj
+++ check-fns/project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/salutem.check-fns "0.1.7-RC14"
+(defproject io.logicblocks/salutem.check-fns "0.1.7-RC15"
   :description "A set of standard check functions for salutem."
 
   :parent-project {:path "../parent/project.clj"
